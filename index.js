98be4df3122110374524b6ff2c55097025ce9216
Samuel Njenga <snjenga704@gmail.com>
2023-08-24 16:05:31 +0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const cookieSession = require("cookie-session");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+require("dotenv").config();
+
+const passportSetup = require("./passport");
+const authRoute = require("./routes/auth");
+
+const app = express();
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["openreplay"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+app.use("/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("server is running!");
+});
