06515ba358a55bf18b796587b69c7bfb783b8ba9
Samuel Njenga <snjenga704@gmail.com>
2023-08-28 12:59:55 +0300

    feat: modify index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cookieSession = require("cookie-session");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 require("dotenv").config();
 
@@ -13,20 +14,24 @@ app.use(
   cookieSession({
     name: "session",
     keys: ["openreplay"],
-    maxAge: 24 * 60 * 60 * 100,
+    maxAge: 24 * 60 * 60 * 1000, // Corrected value (in milliseconds)
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+    origin: "http://localhost:5173", // Corrected: Removed trailing slash
+    credentials: true, // Allow credentials (cookies, etc.) to be sent
   })
 );
+
 app.use("/auth", authRoute);
 
-app.listen("5000", () => {
-  console.log("server is running!");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000!");
 });
